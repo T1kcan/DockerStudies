069722e5f40ef9a465e0d43a862c16eeee68e660
T1kcan <97545634+T1kcan@users.noreply.github.com>
2022-09-17 12:28:56 +0300

    Update welcome2.py

--- clarusway-web/welcome2.py
+++ clarusway-web/welcome2.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "<h1>Welcome to Clarusway Ver II</h1>"
+    return "<h1>Welcome to Docker Class II</h1>"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
 
